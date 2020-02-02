aa45fa5f653be5dadbba77e0dea05b6da3a2fb85
Adam Turner <ll16a6t@leeds.ac.uk>
2020-02-02 18:28:31 +0000

    Compiler now outputs tokens to file (temporary).

--- coursework/src/Compiler.java
+++ coursework/src/Compiler.java
@@ -1,9 +1,9 @@
 import java.io.FileNotFoundException;
-import java.util.List;
+import java.io.*;
 
 public class Compiler {
 
-  public static void main(String[] args){
+  public static void main(String[] args) throws FileNotFoundException{
 
     if (args.length != 1){
       System.err.println("Error. Input file required.");
@@ -19,14 +19,17 @@ public class Compiler {
       e.printStackTrace();
     }
 
-    /*List<String> lines = input.getLines();
-    for (String line : lines){
-      System.out.println(line);
-    }*/
+    PrintStream out = new PrintStream(new File("tests/tokens.txt"));
+
+    PrintStream console = System.out;
+
+    System.setOut(out);
 
     Token t;
     while ((t = input.getNextToken()) != null){
       System.out.println(t);
     }
+
+    System.setOut(console);
   }
 }
--- coursework/src/Lexer.java
+++ coursework/src/Lexer.java
@@ -1,4 +1,3 @@
-import javax.naming.directory.InvalidAttributesException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
