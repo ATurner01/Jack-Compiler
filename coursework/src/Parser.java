efbc32e78b365c8da904ff44dac8708c288a676b
Adam <ll16a6t@leeds.ac.uk>
2020-03-01 23:52:23 +0000

    Implemented first 4 Grammar rules for Parser.

--- coursework/src/Parser.java
+++ coursework/src/Parser.java
@@ -34,23 +34,135 @@ public class Parser {
    * Begins the parsing process from the start variable of the JACK grammar
    */
   public void parse(){
-
+    classDeclare();
   }
 
   private void classDeclare(){
+    Token t = lexer.getNextToken();
+    if (t.getLexeme().equals("class")){
 
+    }
+    else {
+      throw new ParserException("Error on line " + t.getLineNum() + ". " +
+              "Excepted class keyword, got " + t.getLexeme() + ".");
+    }
+
+    t = lexer.getNextToken();
+    if (t.getType() == Token.TokenTypes.id){
+
+    }
+    else {
+      throw new ParserException("Error on line " + t.getLineNum() + ". " +
+              "Expected identifier, got " + t.getLexeme() + ".");
+    }
+
+    t = lexer.getNextToken();
+    if (t.getLexeme().equals("{")){
+
+    }
+    else {
+      throw new ParserException("Error on line " + t.getLineNum() + ". " +
+              "Expected '{', got " + t.getLexeme() + ".");
+    }
+
+    Token next = lexer.peekNextToken();
+    while (!next.getLexeme().equals("}")){
+      memberDeclare();
+      next = lexer.peekNextToken();
+    }
+
+    t = lexer.getNextToken();
+    if (t != null){
+
+    }
+    else {
+      throw new ParserException("Error. Expected closing bracket at end of " +
+              "file, got nothing.");
+    }
+
+    if (t.getLexeme().equals("}")){
+
+    }
+    else {
+      throw new ParserException("Error on line " + t.getLineNum() + ". " +
+              "Expected '}', got" + t.getLexeme() + ".");
+    }
   }
 
-  private void memberDeclate(){
-
+  private void memberDeclare(){
+    Token t = lexer.peekNextToken();
+    if (t.getLexeme().equals("static") || t.getLexeme().equals("field")){
+      classVarDeclare();
+    }
+    else if (t.getLexeme().equals("constructor") || t.getLexeme().equals(
+            "function") || t.getLexeme().equals("method")){
+      subroutineDeclare();
+    }
+    else {
+      throw new ParserException("Error on line " + t.getLineNum() + ". " +
+              "Expected member declaration, got " + t.getLexeme() + ".");
+    }
   }
 
   private void classVarDeclare(){
+    Token t = lexer.getNextToken();
+    if (t.getLexeme().equals("static")){
+      type();
+    }
+    else if (t.getLexeme().equals("field")){
+      type();
+    }
+    else {
+      throw new ParserException("Error on line " + t.getLineNum() + ". " +
+              "Expected \"static\" or \"field\", got " + t.getLexeme() + ".");
+    }
+
+    t = lexer.getNextToken();
+    if (t.getType() == Token.TokenTypes.id){
+
+    }
+    else {
+      throw new ParserException("Error on line " + t.getLineNum() + ". " +
+              "Expected identifier, got " + t.getLexeme() + ".");
+    }
+
+    t = lexer.peekNextToken();
+    while (t.getLexeme().equals(",")){
+      lexer.getNextToken();
+      t = lexer.getNextToken();
+
+      if (t.getType() == Token.TokenTypes.id){
+
+      }
+      else {
+        throw new ParserException("Error on line " + t.getLineNum() + ". " +
+                "Expected identifier, got " + t.getLexeme() + ".");
+      }
+
+      t = lexer.peekNextToken();
+    }
 
   }
 
   private void type(){
+    Token t = lexer.getNextToken();
 
+    if (t.getLexeme().equals("int")){
+
+    }
+    else if (t.getLexeme().equals("char")){
+
+    }
+    else if (t.getLexeme().equals("boolean")){
+
+    }
+    else if (t.getType() == Token.TokenTypes.id){
+
+    }
+    else {
+      throw new ParserException("Error on line " + t.getLineNum() + ". " +
+              "Expected variable type, got " + t.getLexeme() + ". ");
+    }
   }
 
   private void subroutineDeclare(){
