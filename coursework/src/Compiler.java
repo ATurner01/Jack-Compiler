24bb7b5e696d8dd756467c04af4085e581d05794
Adam <ll16a6t@leeds.ac.uk>
2020-03-06 18:47:02 +0000

    Compiler now compiles all files in a given directory.

--- coursework/src/Compiler.java
+++ coursework/src/Compiler.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Compiler {
@@ -28,6 +29,46 @@ public class Compiler {
     System.setOut(console);
   }
 
+  /**
+   * Checks to see whether the provided file has the .jack extension
+   * @param file The path to the source code file
+   * @return True if the file has the .jack extension, otherwise false
+   */
+  public static boolean checkExtension(String file){
+    int extensionStart = file.lastIndexOf('.');
+    if (extensionStart <= 0){
+      System.out.println("Error. File has no extension. Skipping file...");
+      return false;
+    }
+
+    String extension = file.substring(extensionStart + 1);
+    if (!(extension.equals("jack"))){
+      System.out.println("Error. File must have the extension '.jack'. " +
+              "Skipping file...");
+      return false;
+    }
+
+    return true;
+  }
+
+  public static List<String> getFiles(String dirName){
+
+    File dir = new File(dirName);
+    if (!(dir.isDirectory())){
+      System.err.println("Error. Input is not a valid directory.");
+      System.exit(2);
+    }
+
+    File[] listOfFiles = dir.listFiles();
+    ArrayList<String> files = new ArrayList<>();
+
+    for (File file : listOfFiles){
+      files.add(file.getPath());
+    }
+
+    return files;
+  }
+
   public static void main(String[] args) throws FileNotFoundException{
 
     if (args.length != 1){
@@ -35,13 +76,28 @@ public class Compiler {
       System.exit(1);
     }
 
-    Parser parse = new Parser(args[0]);
-    Lexer input = parse.getLexer();
+    System.out.println("Reading in files...");
+    List<String> files = getFiles(args[0]);
 
-    List<String> lines = input.getLines();
-    for (String line : lines){
-      System.out.println(line);
+    for (String file : files) {
+      boolean isCorrectExtension = checkExtension(file);
+
+      if (!isCorrectExtension){
+        continue;
+      }
+
+      System.out.println("Parsing file " + file + "...");
+      Parser parse = new Parser(file);
+//      Lexer input = parse.getLexer();
+//
+//      List<String> lines = input.getLines();
+//      for (String line : lines) {
+//        System.out.println(line);
+//      }
+      System.out.println("Done.");
     }
+
+    System.out.println("Finished.");
 
   }
 }
